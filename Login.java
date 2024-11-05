ef1b67c627b0fc1c3b2275c29a1b34089e5739e6
Rakesh Shaw <rakesh.shaw@wissen.com>
2024-11-05 09:06:10 +0530

    add login

---
+++ Login.java
@@ -0,0 +1,13 @@
+class Login
+{
+	public void login()
+	{
+		System.out.println("Login feature");
+	}
+	public static void main(String[] args)
+	{
+		Login login= new Login();
+		login.login();
+	}
+
+}
