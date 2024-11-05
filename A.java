5ddb6c3df81fe4e5eb587d01e8b9137c05003f2b
Rakesh Shaw <rakesh.shaw@wissen.com>
2024-11-05 09:17:17 +0530

    Initial commit with A and Login classes

--- A.java
+++ A.java
@@ -1,7 +1,5 @@
-public class A
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Have a nice day!!");
-	}
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Have a nice day!!");
+    }
 }
